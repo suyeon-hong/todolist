90eec105447117fadb34b3ba40b9bd11f8a35628
suyeon-hong <bellahong95@gmail.com>
2022-02-03 14:48:30 +0900

    :sparkles: 상단 헤더에 오늘 날짜 제공
    See issues: #2

--- src/components/base/Header/Header.jsx
+++ src/components/base/Header/Header.jsx
@@ -1,10 +1,23 @@
 import * as S from './Style';
 
 export default function Header() {
+  const MONTH = [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ];
+  const date = new Date();
+
   return (
     <>
-      <S.TitleDate>2222년 22월 1</S.TitleDate>
-      <S.Today>수요일</S.Today>
+      <S.TitleDate>
+        {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일
+      </S.TitleDate>
+      <S.Today>{MONTH[date.getDay()]}</S.Today>
       <S.ToDoNums>남은 할 일 0개</S.ToDoNums>
     </>
   );
